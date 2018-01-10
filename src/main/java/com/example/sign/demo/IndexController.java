cb7e5dcc1a1bfcb1ad1e80ea28f71f2d5488f25d
Bohdan <bogdanrybak1996@gmail.com>
2018-01-10 18:13:11 +0200

    init commit

---
+++ src/main/java/com/example/sign/demo/IndexController.java
@@ -0,0 +1,19 @@
+package com.example.sign.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public ModelAndView index() {
+        Map<String, String> model = new HashMap<>();
+        model.put("name", "Bohdan");
+        return new ModelAndView("index",model);
+    }
+}
